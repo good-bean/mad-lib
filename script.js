89bf3e8284b4e76ed49eb2e3590cd39032a8bcca
Bridget F <107891502+good-bean@users.noreply.github.com>
2023-11-01 18:35:51 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+/* Given the 'id' attribute of a form element, 
+   this will return the value entered by the user 
+	 into that form element. */
+function formValue(id) {
+  // ???
+  let formElement = document.getElementById(id);
+  
+  // ???
+  if (formElement === null) {
+    return `[${id} NOT FOUND]`;
+  }
+
+  let value = formElement.value;
+
+	// ???
+  if (formElement.value.trim() === "") {
+    return `[${formElement.placeholder}]`;
+  }
+
+  return document.getElementById(id).value;
+}
+
+/* Given the 'id' of an HTML element and a 
+   'className', this will add that class to
+   the HTML element. */
+function addClassToElement(id, className) {
+  let element = document.getElementById(id);
+  element.classList.add(className);
+}
+
+/* The code here will be executed each time
+   that the form button is clicked. */
+function generate() {
+  // Retrieve form values.
+  let relative = formValue("relative");
+  let adjective1 = formValue("adjective-1");
+  let adjective2 = formValue("adjective-2");
+  let adjective3 = formValue("adjective-3");
+  let famousPerson = formValue("famous-person");
+  let noun = formValue("noun");
+  let dessert = formValue("dessert");
+  let petName = formValue("pet-name");
+
+  // Insert form values into MadLib
+  let madLib = `
+  Dear ${relative},
+  <br><br>
+  I have been having a really ${adjective1} time 
+  at camp. The counselour is ${adjective2} and 
+  the food is ${adjective3}. I met ${famousPerson} 
+  and we quickly became ${noun}. Talk soon!
+  <br><br>
+  Your ${dessert},
+  <br><br>
+  ${petName}
+  `;
+
+  // Output madlib to player
+  console.log(madLib);
+  document.getElementById("output").innerHTML = madLib;
+  addClassToElement("container", "generated");
+
+}
